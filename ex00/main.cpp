ec8c3d9481456240d2292551125b1b08a532dce7
Raphael bourgeat <cosmosraph@gmail.com>
2021-07-27 18:55:21 +0200

    ex01 done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rbourgea <rbourgea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/26 10:25:43 by rbourgea          #+#    #+#             */
-/*   Updated: 2021/07/27 18:28:14 by rbourgea         ###   ########.fr       */
+/*   Updated: 2021/07/27 18:31:01 by rbourgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,3 +43,19 @@ int main()
 
     return (0);
 }
+
+// int main( void ) {
+//     int a = 2;
+//     int b = 3;
+//     ::swap( a, b );
+//     std::cout << "a = " << a << ", b = " << b << std::endl;
+//     std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+//     std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+//     std::string c = "chaine1";
+//     std::string d = "chaine2";
+//     ::swap(c, d);
+//     std::cout << "c = " << c << ", d = " << d << std::endl;
+//     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+//     std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+//     return 0;
+// }
---
+++ ex01/iter.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbourgea <rbourgea@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/27 18:32:16 by rbourgea          #+#    #+#             */
+/*   Updated: 2021/07/27 18:45:47 by rbourgea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+#include <iostream>
+
+template<typename T>
+void iter(T* adress, int length, void(*f)(const T element))
+{
+    for (int i = 0; i < length; i++)
+        f(adress[i]);
+}
+
+template<typename T>
+void display(T array)
+{
+    std::cout << array << std::endl;
+}
+
+#endif
