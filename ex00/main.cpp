06285d730f00dcd7cddb924f00b58572dfbae853
Raphael bourgeat <cosmosraph@gmail.com>
2021-07-27 18:29:21 +0200

    ex00 done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,8 +6,40 @@
 /*   By: rbourgea <rbourgea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/26 10:25:43 by rbourgea          #+#    #+#             */
-/*   Updated: 2021/07/26 10:27:01 by rbourgea         ###   ########.fr       */
+/*   Updated: 2021/07/27 18:28:14 by rbourgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
+
+int main()
+{
+    int a = 42;
+    int b = 0;
+    
+    std::cout << "a: " << a << std::endl;
+    std::cout << "b: " << b << std::endl;
+    swap(a, b);
+    std::cout << "========" << std::endl;
+    std::cout << "a: " << a << std::endl;
+    std::cout << "b: " << b << std::endl;
+    std::cout << "========" << std::endl;
+    std::cout << "max: " << max(a, b) << std::endl;
+    std::cout << "min: " << min(a, b) << std::endl;
+
+    std::cout<< std::endl;
+    float c = 42.0f;
+    float d = 0.0f;
+    
+    std::cout << "c: " << c << std::endl;
+    std::cout << "d: " << d << std::endl;
+    swap(a, b);
+    std::cout << "========" << std::endl;
+    std::cout << "c: " << c << std::endl;
+    std::cout << "d: " << d << std::endl;
+    std::cout << "========" << std::endl;
+    std::cout << "max: " << max(c, d) << std::endl;
+    std::cout << "min: " << min(c, d) << std::endl;
+
+    return (0);
+}
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,7 +6,33 @@
 /*   By: rbourgea <rbourgea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/26 10:25:39 by rbourgea          #+#    #+#             */
-/*   Updated: 2021/07/26 10:25:39 by rbourgea         ###   ########.fr       */
+/*   Updated: 2021/07/27 18:28:39 by rbourgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+T const & max(T const &a, T const &b)
+{
+    return (a > b ? a : b);
+}
+
+template <typename T>
+T const & min(T const &a, T const &b)
+{
+    return (a < b ? a : b);
+}
+
+#endif
